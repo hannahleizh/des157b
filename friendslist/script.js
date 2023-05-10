c7e1912794aef69f8551e43f5a3b95859f90518c
hannah_lei <hlanderson@ucdavis.edu>
2023-05-10 15:50:55 -0700

    new

--- friendslist/script.js
+++ friendslist/script.js
@@ -1,5 +1,5 @@
 // JS here
-Parse.initialize("0rcY5EBhPOX46SxrozcqDnPkEIBpmaOVIeuGiHLB", "9v6nHV3M3HncKhwq8xhtrl6wdpB0K1qtC2tPzh7r"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
+Parse.initialize("0rcY5EBhPOX46SxrozcqDnPkEIBpmaOVIeuGiHLB", "9v6nHV3M3HncKhwq8xhtrl6wdpB0K1qtC2tPzh7r");
 Parse.serverURL = 'https://parseapi.back4app.com/'
 
 
@@ -8,6 +8,54 @@ const editBtns = document.querySelectorAll('.fa-edit');
 const addFriendForm = document.getElementById('add-friend');
 const editFriendForm = document.getElementById('edit-friend');
 const friendList = document.querySelector('main ol');
+
+async function displayFriends() {
+    const friends = Parse.Object.extend('Friends');
+    const query = new Parse.Query(friends);
+    const results = await query.ascending('lname').find();
+    console.log(results);
+
+    try {
+        results.forEach(function (eachFriend) {
+            const id = eachFriend.id;
+            const lname = eachFriend.get('lname');
+            const fname = eachFriend.get('fname');
+            const email = eachFriend.get('email');
+            const facebook = eachFriend.get('facebook');
+            const twitter = eachFriend.get('twitter');
+            const instagram = eachFriend.get('instagram');
+            const linkedin = eachFriend.get('linkedin');
+        
+    
+            // add the lines below...
+            const theListItem = document.createElement('li');
+            theListItem.setAttribute('id', `r-${id}`);
+            theListItem.innerHTML = ` 
+            <div class="name">${fname} ${lname}</div>
+            <div class="email">
+                <i class="fas fa-envelope-square"></i> ${email}
+            </div>
+            <div class="social">
+                <a href="${facebook}"><i class="fab fa-facebook-square"></i></a>
+                <a href="${twitter}"><i class="fab fa-twitter-square"></i></a>
+                <a href="${instagram}"><i class="fab fa-instagram"></i></a>
+                <a href="${linkedin}"><i class="fab fa-linkedin"></i></a>
+            </div>
+            <i class="fas fa-edit" id="e-${id}"></i>
+            <i class="fas fa-times-circle" id="d-${id}"></i> `;
+    
+            friendList.append(theListItem);
+        });
+    
+    } catch (error) {
+        console.error('Error while fetching friends', error);
+    }
+    
+    
+}
+
+displayFriends();
+
 
 newBtn.addEventListener('click', function (event) {
     event.preventDefault();
@@ -30,49 +78,3 @@ editFriendForm.addEventListener('submit', function (event) {
     event.preventDefault();
     editFriendForm.className = "edit-friend-offscreen";
 });
-
-async function displayFriends() {
-    const friends = Parse.Object.extend('Friends');
-    const query = new Parse.Query(friends);
-    const results = await query.ascending('lname').find();
-    // console.log(results);
-}
-
-// displayFriends();
-
-try {
-    results.forEach(function (eachFriend) {
-        const id = eachFriend.id;
-        const lname = eachFriend.get('lname');
-        const fname = eachFriend.get('fname');
-        const email = eachFriend.get('email');
-        const facebook = eachFriend.get('facebook');
-        const twitter = eachFriend.get('twitter');
-        const instagram = eachFriend.get('instagram');
-        const linkedin = eachFriend.get('linkedin');
-    
-
-        // add the lines below...
-        const theListItem = document.createElement('li');
-        theListItem.setAttribute('id', `r-${id}`);
-        theListItem.innerHTML = ` 
-                <div class="name">${fname} ${lname}</div>
-                <div class="email">
-                    <i class="fas fa-envelope-square"></i> ${email}
-                </div>
-                <div class="social">
-                    <a href="${facebook}"><i class="fab fa-facebook-square"></i></a>
-                    <a href="${twitter}"><i class="fab fa-twitter-square"></i></a>
-                    <a href="${instagram}"><i class="fab fa-instagram"></i></a>
-                    <a href="${linkedin}"><i class="fab fa-linkedin"></i></a>
-                </div>
-                <i class="fas fa-edit" id="e-${id}"></i>
-                <i class="fas fa-times-circle" id="d-${id}"></i> `
-
-        friendList.append(theListItem);
-    });
-
-} catch (error) {
-    console.error('Error while fetching friends', error);
-}
-
