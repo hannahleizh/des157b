4727faa2efe862ed797f7dff361eb693e4c1fa86
hannah_lei <hlanderson@ucdavis.edu>
2023-05-31 17:55:44 -0700

    new

---
+++ capstone/development/usability/script.js
@@ -0,0 +1,6 @@
+(function() {
+    
+    // alert for usability
+    alert("Hi there! Thanks for taking the time to check out my Davis Puzzle Game. This project is still very much under construction, and most parts are not working yet. I will open up any pages for you when it makes sense. Besides that, I'd like you to navigate the site, and imagine yourself as a 4th grader learning more about Davis and completing the Puzzle.");
+    
+})();
